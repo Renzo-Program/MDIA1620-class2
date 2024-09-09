67e5f1f2f7248889de3b10c0dec7f404cc5ab343
Renzo <renzo.teves15@gmail.com>
2024-09-09 11:12:05 -0700

    cooleness attempt

--- class2git/test.js
+++ class2git/test.js
@@ -1,11 +1,20 @@
 console.log("Welcome to the Amazing Metal Person");
 console.log("Welcome to the Amazing Spider Person");
-console.log("x1b[32m","Hi")
-
+console.log("x1b[30m","Hi")
 console.log("x1b[31m","Hi")
-console.log("x1b[37m","Hi")
+console.log("x1b[32m","Hi")
 console.log("x1b[33m","Hi")
-console.log("x1b[0m","Hi")
+console.log("x1b[34m","Hi")
+console.log("x1b[35m","Hi")
+console.log("x1b[36m","Hi")
+console.log("x1b[37m","Hi")
 
-console.log("x1b[45m","Hi")
+console.log("\x1b[0m","Hi")
 console.log("x1b[40m","Hi")
+console.log("x1b[41m","Hi")
+console.log("x1b[42m","Hi")
+console.log("x1b[43m","Hi")
+console.log("x1b[44m","Hi")
+console.log("x1b[45m","Hi")
+console.log("x1b[46m","Hi")
+console.log("x1b[47m","Hi")
