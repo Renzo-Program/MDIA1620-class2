6c6acf6542525ca9ecaf203758c5600c1ac0ed06
Renzo <renzo.teves15@gmail.com>
2024-09-09 11:06:15 -0700

    cooleness log

--- class2git/test.js
+++ class2git/test.js
@@ -1,2 +1,11 @@
 console.log("Welcome to the Amazing Metal Person");
 console.log("Welcome to the Amazing Spider Person");
+console.log("x1b[32m","Hi")
+
+console.log("x1b[31m","Hi")
+console.log("x1b[37m","Hi")
+console.log("x1b[33m","Hi")
+console.log("x1b[0m","Hi")
+
+console.log("x1b[45m","Hi")
+console.log("x1b[40m","Hi")
